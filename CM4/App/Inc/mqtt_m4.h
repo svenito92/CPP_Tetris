5fd447dd35f6af857c42bf641874a14eebb95308
Sven Wanzenried <sven.wanzenried@gmail.com>
2022-06-17 11:51:15 +0200

    MQTT: Connection, subscribe and publish working

---
+++ CM4/App/Inc/mqtt_m4.h
@@ -0,0 +1,35 @@
+/*
+ * mqtt_m4.h
+ *
+ *  Created on: 17 Jun 2022
+ *      Author: Sven
+ */
+
+#ifndef INC_MQTT_M4_H_
+#define INC_MQTT_M4_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "mqtt.h"
+#include "lwip.h"
+
+#define MQTT_M4__RETAIN     1
+#define MQTT_M4__NO_RETAIN  0
+#define MQTT_M4__QOS_MAX_ONCE 0
+#define MQTT_M4__QOS_MIN_ONCE 1
+#define MQTT_M4__QOS_EXACT_ONCE 2
+
+
+void mqtt_m4__init(ip_addr_t host, uint16_t port, const char *client_id);
+void mqtt_m4__connect();
+void mqtt_m4__subscribe(const char *topic, uint8_t qos);
+void mqtt_m4__publish(const char *topic, uint8_t *buf, uint16_t len, uint8_t qos, uint8_t retain);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_MQTT_M4_H_ */
--- CM4/App/Src/main_m4.cpp
+++ CM4/App/Src/main_m4.cpp
@@ -10,11 +10,18 @@
 #include "gpio.h"
 #include "lwip.h"
 
+
+#include "mqtt_m4.h"
+
+#define DHCP_STATE_BOUND  10
+
 #ifndef HSEM_ID_0
 #define HSEM_ID_0 (0U) /* HW semaphore 0*/
 #endif
 
 void bootSystem(void);
+
+volatile uint32_t msTime;
 
 int main(void)
 {
@@ -24,9 +31,30 @@ int main(void)
   MX_GPIO_Init();
   MX_USART3_UART_Init();
   MX_LWIP_Init();
+  MX_LWIP_Process();
+
+  // Wait sometime for dhcp bind
+  while (ethernetif_dhcp_state != DHCP_STATE_BOUND)
+  {
+    MX_LWIP_Process();
+  }
+//  uint32_t timestamp = HAL_GetTick();
+//  while (HAL_GetTick() < timestamp + 10000)
+//  {
+//    MX_LWIP_Process();
+//  }
+
+  ip_addr_t host;
+  IP4_ADDR(&host, 10, 20, 30, 1);
+
+  mqtt_m4__init(host, 1883, "tetris_test");
+  mqtt_m4__connect();
+  mqtt_m4__subscribe("test_subscribe", 0);
+  mqtt_m4__publish("tetris_publish", (uint8_t*)&host , sizeof(ip_addr_t), MQTT_M4__QOS_MAX_ONCE, MQTT_M4__NO_RETAIN);
 
   while (1)
   {
+    msTime = HAL_GetTick();
     MX_LWIP_Process();
   }
 }
